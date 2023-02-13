fc21b34142f711188251c75a3ce97b36914dd90e
suryahadiwinata1 <125232702+suryahadiwinata1@users.noreply.github.com>
2023-02-13 09:48:49 +0700

    Add files via upload

---
+++ latihan_2.py
@@ -0,0 +1,17 @@
+'''buatlah program menghitung luas bangun datar menggunakan fungsi. gunakan parameter
+- segitiga
+- persegi panjang
+'''
+
+def segitiga(alas,tinggi):
+    luas = alas * tinggi / 2
+    print("luas segitiga adalah:%i cm"%luas)
+
+segitiga(30, 40)
+
+
+def persegi_panjang(panjang,lebar):
+    luas = panjang * lebar
+    print("luas persegi panjang adalah:%i cm"%luas)
+
+persegi_panjang(59, 80)
---
+++ latihan_fungsi.py
@@ -0,0 +1,47 @@
+'''fungsi pada python didefinisikan dengan def'''
+
+'''def nama_fungsi():'''
+
+def salam():
+  print("assalamu'alaikum' !! akhi ukhty")
+
+salam()
+salam()
+salam()
+
+'''buatlah sebuah fungsi versi kalian'''
+
+def sokin():
+  print("sokin paris")
+
+sokin()
+sokin()
+sokin()
+
+'''fungsi dengan parameter'''
+'''def dengan nama_fungsi(p1, p2)'''
+
+def say_hi(nama):
+  print("halo perkenalkan nama saya", nama)
+
+say_hi("jawir")
+say_hi("dower")
+
+'''buatlah fungsi dengan 3 parameter'''
+
+def ITZY(ITZY1, ITZY2, ITZY3):
+  print("halo perkenalkan nama istri saya yang pertama" + " " + ITZY1)
+  print("halo perkenalkan nama istri saya yang kedua" + " " + ITZY2)
+  print("halo perkenalkan nama istri saya yang ketiga" + " " + ITZY3)
+  
+ITZY(ITZY1 = "Yeji", ITZY2 = "Yuna", ITZY3 = "Ryujin")
+
+
+def perkenalkan(nama, alamat, umur):
+  print("nama saya:", nama)
+  print("alamat saya:", alamat)
+  print("umur saya:", umur, 'tahun')
+
+perkenalkan("Yeji", "korea", 22)
+
+print("\n\n========== BATAS ==========")
